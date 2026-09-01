9a4f49f4b1c3effa6e7581efadf7d90bde424dfd
digi-bots <akil.ahmed.digi@gmail.com>
2026-09-02 03:12:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")   # Render-এ Environment Variable থেকে নেবে
+BOT_TOKEN = os.getenv("BOT_TOKEN")          # Render-এ Environment Variable থেকে নেবে
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "1604189136").split(",")]
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "usdt_repay")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1004343332067")) if os.getenv("CHANNEL_ID") else None
